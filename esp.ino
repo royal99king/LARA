f5c21d64b835e2d1a3da008e6ce48db16b6dfc59
Prashant Uniyal <36094606+royal99king@users.noreply.github.com>
2018-09-02 20:35:59 +0530

    Configure Esp8266

---
+++ esp.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+SoftwareSerial esp8266(10, 11); // make RX Arduino line is pin 10, make TX Arduino line is pin 11.
+void setup()
+{
+  Serial.begin(115200);
+  esp8266.begin(115200); // esp's baud rate
+}
+void loop()
+{
+  if (esp8266.available())
+  {
+    while (esp8266.available())
+    {
+      char c = esp8266.read();
+      Serial.write(c);
+    }
+  }
+  if (Serial.available())
+  {
+    delay(2000);
+    String command = "";
+    while (Serial.available())
+    {
+      // read one character
+      command += (char)Serial.read();
+      
+    }
+    esp8266.println(command);
+    Serial.println("COMMAND : " + command);
+    
+  }
+}
